2e32d626577eca99765016e47240d0ebf707fc05
Florian <florian.sasso@outlook.fr>
2023-03-16 16:41:35 +0100

    Cart last for working_branch only

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -5,17 +5,16 @@ class CartController < ApplicationController
 
   def add
     @item = Item.find_by(id: params[:id])
-    quantity = params[:quantity].to_i
     current_order = @cart.orders.find_by(item_id: @item.id)
-    if current_order && quantity > 0
-      current_order.update(quantity:)
+    if current_order
+      current_order.update
     elsif quantity <= 0
       current_order.destroy
     else
       @cart.orders.create(item: @item, quantity:)
     end
   end
-  
+
   def remove
     Order.find_by(id: params[:id]).destroy
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,5 @@ class Order < ApplicationRecord
   belongs_to :item
   belongs_to :cart
 
-  def total
-    item.price * quantity
-  end
 
 end
